3af414096dce05776d706d6151ba0b4142396bf8
Dror Maor <dror.m.maor@gmail.com>
2019-10-23 08:34:20 -0400

    cron split

--- admin/cron.php
+++
@@ -1,5 +0,0 @@
-<?php
-	include("MLB.php");
-	include("NHL.php");
-	include("NBA.php");
-?>
--- csv.php
+++ csv.php
@@ -1,4 +1,5 @@
 <?php
+	mail("dror.m.maor@gmail.com", "BPP csv", "");
 
 //	error_reporting(E_ALL);
 //	ini_set('display_errors', 1);
